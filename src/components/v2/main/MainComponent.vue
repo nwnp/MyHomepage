fc469cf943dea0c7dd979e9709dc08a4ef4c8fc9
nwnp <jjw_0418@naver.com>
2022-11-23 20:13:53 +0900

    feat: PostDetail 구현중

--- src/components/v2/main/MainComponent.vue
+++ src/components/v2/main/MainComponent.vue
@@ -12,7 +12,7 @@
               v-else
               v-for="(post, id) in getLimitedPosts"
               :key="(post, id)"
-              @click="clickedPost(post.id)"
+              @click="clickedPost(id, 'DETAIL')"
             >
               <div class="post-title">📌 {{ post.title }}</div>
               <div class="post-content">{{ post.content }}</div>
@@ -30,17 +30,34 @@
         </div>
       </div>
     </div>
+    <PostDetailModal
+      v-if="postDetailModal"
+      class="post-detail-modal"
+      :postInfo="postInfo"
+      @closeModal="closeModal"
+    />
   </div>
 </template>
 
 <script>
 import { Query } from "@/apollo/query/query";
 import { getCookie } from "@/functions/getCookie";
+import PostDetailModal from "@/components/v2/post/PostDetailModal.vue";
 
 export default {
+  components: {
+    PostDetailModal,
+  },
   data() {
     return {
       getLimitedPosts: "",
+      postDetailModal: false,
+      postInfo: {
+        modalType: "",
+        title: "",
+        content: "",
+        PostId: "",
+      },
     };
   },
   apollo: {
@@ -57,8 +74,17 @@ export default {
     },
   },
   methods: {
-    clickedPost(postId) {
-      console.log("clicked post", postId);
+    clickedPost(id, type) {
+      this.postInfo = {
+        modalType: type,
+        title: this.getLimitedPosts[id].title,
+        content: this.getLimitedPosts[id].content,
+        PostId: this.getLimitedPosts[id].id,
+      };
+      this.postDetailModal = !this.postDetailModal;
+    },
+    closeModal() {
+      this.postDetailModal = !this.postDetailModal;
     },
   },
 };
@@ -129,5 +155,14 @@ li {
   margin: 8px 0px 0px 10px;
   display: flex;
   align-items: center;
+}
+
+.post-detail-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
 }
 </style>
