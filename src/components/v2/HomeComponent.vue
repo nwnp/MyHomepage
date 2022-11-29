c0b5f1d5d6297761d42386e643f016cd80c1d466
nwnp <jjw_0418@naver.com>
2022-11-30 01:44:55 +0900

    style: HomeComponent & SignupComponent

--- src/components/v2/HomeComponent.vue
+++ src/components/v2/HomeComponent.vue
@@ -1,60 +1,28 @@
 <template>
-  <section class="container">
-    <div class="home-container">
+  <div class="home-container">
+    <div>my hompy</div>
+    <form @submit.prevent="submitLogin">
       <div>
-        <h2 class="title">LOGIN</h2>
-      </div>
-      <div class="form">
-        <form @submit.prevent="submitLogin" class="form">
-          <div class="widget">
-            <label for="email">
-              <b> Email </b>
-            </label>
-            <input
-              type="text"
-              id="email"
-              v-model="email"
-              placeholder="깃허브 로그인을 네이버 로그인으로 수정"
-            />
-          </div>
-          <div class="widget">
-            <label for="password">
-              <b>Password</b>
-            </label>
-            <input
-              type="password"
-              id="password"
-              v-model="password"
-              placeholder="페이스북 로그인을 카카오 로그인으로 수정"
-            />
-          </div>
-          <button>로그인하기</button>
-        </form>
+        <input type="text" id="email" v-model="email" placeholder="이메일" />
+        <label for="email" />
       </div>
       <div>
-        <button>비회원으로 로그인하기</button>
+        <input
+          type="password"
+          id="password"
+          v-model="password"
+          placeholder="비밀번호"
+        />
+        <label for="password" />
       </div>
-      <div class="socials">
-        <div class="facebook">
-          <i class="fa-brands fa-facebook"></i>
-        </div>
-        <div class="google">
-          <i class="fa-brands fa-google"></i>
-        </div>
-        <div class="github">
-          <i class="fa-brands fa-github"></i>
-        </div>
-      </div>
-      <div class="links">
-        <div class="signup">
-          <router-link to="/signup">SIGNUP</router-link>
-        </div>
-        <div class="forgot-password">
-          <b> FORGOT PASSWORD? </b>
-        </div>
-      </div>
+      <button style="submit">로그인</button>
+    </form>
+    <div>
+      <div>이메일 찾기</div>
+      <div>비밀번호 찾기</div>
+      <router-link to="/signup">회원가입</router-link>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -101,82 +69,9 @@ export default {
 
 <style scoped>
 .home-container {
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  border-radius: 15px;
-  width: 300px;
-  height: 500px;
-  box-shadow: 1px 3px 3px rgba(0, 0, 0, 0.5);
-  justify-content: space-between;
-  padding: 2em;
-}
-
-.title {
-  font-size: 2em;
-  text-align: center;
-}
-
-.widget {
-  display: flex;
-  flex-direction: column;
-  row-gap: 0.5em;
-  margin-bottom: 1em;
-}
-
-input {
-  border: none;
-  outline: none;
-  border-bottom: 1px solid #e5e5e5;
-  padding: 0.5em 0;
-}
-
-button {
-  cursor: pointer;
-  width: 100%;
-  border-radius: 15px;
-  height: 35px;
-  background-color: #3498db;
-  color: white;
-  margin-top: 1em;
-  border: none;
-  transition: all, 0.5s;
-}
-
-button:hover {
-  background-color: #60b6f0;
-}
-
-i {
-  font-size: 2.5em;
-}
-
-.facebook {
-  color: #2980b9;
-}
-
-.google {
-  color: #e74c3c;
-}
-
-.github {
-  color: #34495e;
-}
-
-.socials {
-  display: flex;
-  justify-content: space-around;
-  column-gap: 1em;
-}
-
-.links signup {
-  color: #34495e;
-  font-size: 85%;
-}
-
-.links {
-  display: flex;
-  justify-content: space-between;
+  height: 100%;
+  padding: 10px;
+  background-color: #fff;
 }
 
 a {
